36e769fdfcc19a0974ef4d11be833eefb788f506
Jack Xu <yurusanP@gmail.com>
2023-02-07 23:23:15 -0600

    first commit

---
+++ build.gradle.kts
@@ -0,0 +1,12 @@
+plugins {
+  idea
+}
+
+allprojects {
+  group = "org.semgusng"
+  version = "0.1.0.0"
+
+  repositories {
+    mavenCentral()
+  }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "semgus-ng"
